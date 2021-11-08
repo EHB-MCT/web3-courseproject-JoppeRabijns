ea93200139b9425194e1f2f654531fb7c4a9570b
Wouter Heirstrate <wouter.heirstrate@gmail.com>
2021-11-08 15:46:26 +0100

    changes

---
+++ nexrender/frontend/src/scripts/Preloader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Preloader.css';
+
+export default function Preloader() {
+  return(
+    <div className="preloader">
+      <p className="appTitle">
+        Pitch:fx
+      </p>
+    </div>
+  )
+}
