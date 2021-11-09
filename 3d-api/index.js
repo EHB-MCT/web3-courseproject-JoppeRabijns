946dceb7f059a3027ad8aa18c1e603a6518c7be9
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-11-09 10:18:14 +0100

    process.env.PORT

--- 3d-api/index.js
+++ 3d-api/index.js
@@ -3,7 +3,7 @@ const routes = require('./routes/model');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use('/uploads', express.static('./uploads'));
