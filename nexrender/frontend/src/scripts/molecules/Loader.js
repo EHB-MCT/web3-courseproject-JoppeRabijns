b6d082126acc31e1ddafcab4b7c0545202009e6e
Wouter Heirstrate <wouter.heirstrate@gmail.com>
2021-11-10 10:39:37 +0100

    feat(FE): remove unused imports

--- nexrender/frontend/src/scripts/molecules/Loader.js
+++ nexrender/frontend/src/scripts/molecules/Loader.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Loader from "react-loader-spinner";
 import { useNavigate } from "react-router-dom";
 
--- nexrender/frontend/src/scripts/pages/Preloader.js
+++ nexrender/frontend/src/scripts/pages/Preloader.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import CustomLoader from '../molecules/Loader';
 
 import './styles/Preloader.css'
