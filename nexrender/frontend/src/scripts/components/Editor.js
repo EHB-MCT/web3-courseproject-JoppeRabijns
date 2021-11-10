9cabac167e457c353245633cbdb2b1797b3e802d
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-11-10 11:14:49 +0100

    feat(videoContext): added videocontext package

--- nexrender/frontend/src/scripts/components/Editor.js
+++ nexrender/frontend/src/scripts/components/Editor.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import VideoContext from "videocontext";
 
-import './styles/Editor.css';
-
-export default function Editor(){
-  return(
-    <p>Editor</p>
-  )
+class Editor extends React.Component {
+  componentDidMount(){
+  var canvasElement = document.getElementById("canvas");
+  var videoCtx = new VideoContext(canvasElement);
+  var videoNode1 = videoCtx.video(
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+  );
+  videoNode1.start(0);
+  videoNode1.stop(4);
+  
+  var videoNode2 = videoCtx.video(
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+  );
+  videoNode2.start(2);
+  videoNode2.stop(6);
+  
+  var crossFade = videoCtx.transition(VideoContext.DEFINITIONS.CROSSFADE);
+  crossFade.transition(2, 4, 0.0, 1.0, "mix");
+  
+  videoNode1.connect(crossFade);
+  videoNode2.connect(crossFade);
+  crossFade.connect(videoCtx.destination);
+  
+  videoCtx.play();
+  
+  }
+  render() { 
+    return <div id="canvas" style={{height:800, width: 1200}}></div>;
+  }
 }
+ 
+export default Editor;
+
---
+++ nexrender/frontend/src/scripts/molecules/Canvas.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/Canvas.css'
+
+class Canvas extends React.Component {
+  render() { 
+    return <div></div>;
+  }
+}
+ 
+export default Canvas;
