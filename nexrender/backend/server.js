c7b080bb16c29bd39889aceb70ada3b5b498cfa4
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-11-22 14:10:38 +0100

    added lowerthird nexrender post

--- nexrender/backend/server.js
+++ nexrender/backend/server.js
@@ -4,6 +4,7 @@ const CORS = require('cors');
 const APP = EXPRESS();
 const { checkAddVideoBody } = require('./helpers/helper');
 const CLOUDINARY = require('./config/cloudinary');
+const { render } = require('@nexrender/core')
 
 // MIDDLEWARE
 APP.use(CORS());
@@ -18,6 +19,73 @@ APP.get('/', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.send('Hello');
 });
+
+
+APP.post('/lowerthirds/:id', (req, res) => {
+  if(req.params.id == 1){
+    renderLT("https://res.cloudinary.com/pitch-fx/raw/upload/v1637577241/LT/LT1_ngyugs.aep","LT1","LT1->LT_01->LT01_Line1->Text01_LT01","LT1->LT_01->LT01_Line2->Text02_LT01", req)
+  }else if(req.params.id == 2){
+    renderLT("https://res.cloudinary.com/pitch-fx/raw/upload/v1637577240/LT/LT2_i3zbqc.aep","LowerThird_08","LowerThird_08->Lower third 08","LowerThird_08->Lower third 08", req)
+  }else if(req.params.id == 3){
+    renderLT("https://res.cloudinary.com/pitch-fx/raw/upload/v1637577240/LT/LT3_xwtmiv.aep","Snow LT maincomp","Snow LT maincomp->Lower Third","Snow LT maincomp->Lower Third", req)
+  }
+  res.sendStatus(200);
+});
+
+/** */
+async function renderLT(url, comp, name, subtext, req){
+const result = await render({
+  "template": {
+      "src": `${url}`,
+      "composition": `${comp}`,
+  },
+  "assets": [
+      {
+          "type": "data",
+          "layerName": "name",
+          "property": "Source Text",
+          "value": `${req.body.name}`,
+          "composition": `${name}`
+      },
+      {
+          "type": "data",
+          "layerName": "title",
+          "property": "Source Text",
+          "value": `${req.body.title}`,
+          "composition": `${subtext}`
+      }
+  ],
+  "actions":{
+      "predownload": [
+          {
+              "module": "@nexrender/action-cache",
+              "cacheDirectory": "/Users/joppe.rabijns/WEB3/nexrender/cache"
+          }
+      ],
+        "postdownload": [
+          {
+              "module": "@nexrender/action-cache",
+              "cacheDirectory": "/Users/joppe.rabijns/WEB3/nexrender/cache"
+          }
+      ],
+      "postrender": [
+          {
+              "module": "@nexrender/action-encode",
+              "preset": "mov",
+              "output": "encoded.mov"
+          },
+          {
+              "module": "@nexrender/action-copy",     
+              "output": `/Users/joppe.rabijns/WEB3/nexrender/outputs/encoded${Math.random()*100}.mov`
+          }
+      ]
+  }
+}
+, {
+  binary: '/Applications/AdobeAfterEffects/aerender',
+  skipCleanup: true,
+})
+  }
 
 APP.post('/uploadVideo', (req, res) => {
   console.log(req);
