1d996fce704922f37e5eb6f6a20e06aa31b790ab
Wouter Heirstrate <wouter.heirstrate@gmail.com>
2021-12-03 17:25:42 +0100

    TODO met axios

--- nexrender/frontend/src/scripts/components/VideoUpload.js
+++ nexrender/frontend/src/scripts/components/VideoUpload.js
@@ -6,6 +6,12 @@ import "react-dropzone-uploader/dist/styles.css";
 const axios = require("axios").default;
 
 export default function VideoUpload() {
+  function uploadFiles(e, data) {
+    e.preventDefault();
+    console.log("event", e);
+    console.log("data", data);
+  }
+
   return (
     <div className="upload">
       <form
@@ -13,6 +19,7 @@ export default function VideoUpload() {
         encType="multipart/form-data"
         action="http://localhost:5000/uploadVideo"
         method="post"
+        onSubmit={uploadFiles}
       >
         <label htmlFor="model"> 3d model </label>
         <input
