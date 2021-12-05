99e27c68c5dfc3d8af4746113f4025c1303c7f0f
Wouter Heirstrate <wouter.heirstrate@gmail.com>
2021-12-05 23:12:45 +0100

    fix

--- nexrender/frontend/src/scripts/components/VideoUpload.js
+++ nexrender/frontend/src/scripts/components/VideoUpload.js
@@ -5,38 +5,41 @@ import "../shared/shared.css";
 
 export default function VideoUpload({ previous, next }) {
   return (
-    <div className="componentContainer">
-      <h1 className="createTitle">Add your video tracks</h1>
-      <form
-        id="uploadForm"
-        encType="multipart/form-data"
-        action="http://localhost:5000/uploadVideo"
-        method="post"
-      >
-        <label htmlFor="videoFiles"> Upload your video's here </label>
-        <br />
-        <input
-          type="file"
-          name="videoFiles"
-          id="videoFiles"
-          multiple
-          accept="video/*"
-        />
-        <br />
-        <input
-          hidden
-          type="text"
-          name="title"
-          value={sessionStorage.getItem("projectName")}
-          readOnly
-        />
-        <br />
-        <input type="submit" value="Upload asset" name="submit" />
-      </form>
-      <div className="buttonContainer">
-        <CustomButton onClick={previous} value="go back" />
-        <CustomButton onClick={next} value="next" />
+    <>
+      <div className="componentContainer">
+        <h1 className="createTitle">Add your video tracks</h1>
+        <form
+          className="uploadForm"
+          id="uploadForm"
+          encType="multipart/form-data"
+          action="http://localhost:5000/uploadVideo"
+          method="post"
+        >
+          <label className="fileLabel" htmlFor="videoFiles">
+            Click here to upload your files
+          </label>
+          <input
+            className="fileInput"
+            type="file"
+            name="videoFiles"
+            id="videoFiles"
+            multiple
+            accept="video/*"
+          />
+          <input
+            hidden
+            type="text"
+            name="title"
+            value={sessionStorage.getItem("projectName")}
+            readOnly
+          />
+          <CustomButton type="submit" value="Upload" name="submit" />
+        </form>
+        <div className="buttonContainer">
+          <CustomButton onClick={previous} value="go back" />
+          <CustomButton onClick={next} value="next" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
