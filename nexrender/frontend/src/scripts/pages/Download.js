aa1b50094010f23f10ab179991028a9679588daf
Wouter Heirstrate <wouter.heirstrate@gmail.com>
2021-12-06 10:03:22 +0100

    fix download

--- nexrender/frontend/src/scripts/pages/Download.js
+++ nexrender/frontend/src/scripts/pages/Download.js
@@ -1,18 +1,20 @@
 import React from "react";
 import CustomButton from "../molecules/Button";
 import "./styles/Download.css";
+import "../shared/shared.css";
 
 export default function Download() {
   let video = sessionStorage.getItem("url");
 
   return (
-    <>
+    <div className="componentContainer">
       <video width="750" height="500" controls>
         <source src={video} type="video/mp4" />
       </video>
+      <br />
       <a href={video} download>
-        <CustomButton value="Download your video" />
+        <CustomButton value="Download" />
       </a>
-    </>
+    </div>
   );
 }
