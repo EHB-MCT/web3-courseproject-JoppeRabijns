9c126412fadc66a379cfb4fb18e32beac68fa385
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-12-08 10:58:53 +0100

    added router

--- portfolio/src/app.js
+++ portfolio/src/app.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./pages/home";
+import Project from "./pages/project";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/project/:id" component={Project} />
       </Switch>
     </Router>
   );
--- portfolio/src/components/ProjecTitle/ProjectTitle.js
+++ portfolio/src/components/ProjecTitle/ProjectTitle.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./ProjectTitle.css";
+import { Link } from "react-router-dom";
 
 const ProjectTitle = (props) => {
   const [active, setActive] = useState(false);
@@ -16,18 +17,25 @@ const ProjectTitle = (props) => {
         setActive(false);
       }}
     >
-      <div>
-        <div className="smalls">
-          <h6 className="projectNumber">{props.number}</h6>
-          {props.new === "true" ? <h6 className="projectNew">Nieuw</h6> : <></>}
+      <Link to={`/project/${props.number}`} className="link">
+        <div>
+          <div className="smalls">
+            <h6 className="projectNumber">{props.number}</h6>
+            {props.new === "true" ? (
+              <h6 className="projectNew">Nieuw</h6>
+            ) : (
+              <></>
+            )}
+          </div>
+
+          <h1
+            className={active ? "projectTitle active" : "projectTitle inactive"}
+          >
+            {props.title}
+          </h1>
         </div>
-        <h1
-          className={active ? "projectTitle active" : "projectTitle inactive"}
-        >
-          {props.title}
-        </h1>
-      </div>
-      <h3 className="projectCategory">{props.category}</h3>
+        <h3 className="projectCategory">{props.category}</h3>
+      </Link>
     </div>
   );
 };
