ab1ff7ce52d376282a370119b62bbf3173379fbe
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-12-08 13:52:13 +0100

    project number svg line

--- portfolio/src/components/ProjectNumber/ProjectNumber.js
+++ portfolio/src/components/ProjectNumber/ProjectNumber.js
@@ -5,7 +5,11 @@ const ProjectNumber = (props) => {
   return (
     <div>
       <h6 className="ProjectNumber">
-        {props.number} ---- 0{props.numberLenght}
+        {props.number} &nbsp;
+        <svg height="5" width="30">
+          <line x1="0" y1="0" x2="30" y2="0" />
+        </svg>{" "}
+        &nbsp; 0{props.numberLenght}
       </h6>
     </div>
   );
