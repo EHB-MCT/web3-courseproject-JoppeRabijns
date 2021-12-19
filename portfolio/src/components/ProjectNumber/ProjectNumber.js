52bcffb11ba82afae0aac7d868a494a4a655d5c1
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2021-12-19 21:04:04 +0100

    added project and fixed projectnumber

--- portfolio/src/components/ProjectNumber/ProjectNumber.js
+++ portfolio/src/components/ProjectNumber/ProjectNumber.js
@@ -8,8 +8,10 @@ const ProjectNumber = (props) => {
         {props.number} &nbsp;
         <svg height="5" width="30">
           <line x1="0" y1="0" x2="30" y2="0" />
-        </svg>{" "}
-        &nbsp; 0{props.numberLenght}
+        </svg>
+        {props.numberLenght < 10
+          ? ` 0${props.numberLenght}`
+          : ` ${props.numberLenght}`}
       </h6>
     </div>
   );
